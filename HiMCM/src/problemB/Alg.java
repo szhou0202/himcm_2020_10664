6dd39425f6e565bfa4fd1ff095350f45912d232d
Rhuel67 <chrisrwalsh67@gmail.com>
2020-11-12 11:48:59 -0500

    adding plant class

--- HiMCM/src/problemB/Alg.java
+++ HiMCM/src/problemB/Alg.java
@@ -2,6 +2,8 @@ package problemB;
 
 public class Alg{
 	public static void main(String[] args) {
-		
+		double[][] data = new FileReader("fileName").read();
 	}
+	
+	//public double[][][] find groups
 }
---
+++ HiMCM/src/problemB/Plant.java
@@ -0,0 +1,17 @@
+package problemB;
+
+import java.util.ArrayList;
+
+public class Plant {
+	private double ben;
+	private double tax;
+	private double feas;
+	private ArrayList<Double> costs;
+	
+	public Plant(double[] data) {
+		
+	}
+	
+	
+	
+}
